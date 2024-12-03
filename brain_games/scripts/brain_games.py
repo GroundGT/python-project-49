7ff106765075b29ff9e5ad3e890bfeb405e5afc1
Ground <groundgtx@gmail.com>
2024-12-03 21:31:53 +0300

    Stage 4 passed

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
+import prompt
+
+
 def greet():
     print('Welcome to the Brain games')
 
+
+def welcome_user() -> str:
+    name = prompt.string('May I have your name? ')
+    return f'Hello,{name}'
+
+
 def main():
     greet()
+    print(welcome_user())  # Уточнить как правильно
+
 
 if __name__ == '__main__':
     main()
