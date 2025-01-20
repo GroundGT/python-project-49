e63172500c378989ea5494fa397c5e225cb28526
Ground <groundgtx@gmail.com>
2025-01-20 22:18:25 +0300

    Refactor gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,32 +4,40 @@ START_GAP = 1
 END_GAP = 50
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
-
-def make_task() -> tuple[str, str]:
-    """ Generates two random numbers and finds their greatest common divisor
-    (GCD).
-
-    The function creates two random numbers in a given range and calculates
-    their NODE is using the Euclidean algorithm.
+def generate_random_numbers() -> tuple[int, int]:
+    """Generates two random numbers within the specified range.
 
     Returns:
-        tuple[str, str]: A tuple of two elements:
-    - a string with two numbers separated by a space (for example, "15 25")
-    - a string with their largest common divisor
-
-    Note:
-        The algorithm uses the method of sequential division with remainder
-    (Euclid's algorithm) to find the nodes of two numbers.
+        tuple[int, int]: A tuple containing two random integers.
     """
     first_number = randint(START_GAP, END_GAP)
     second_number = randint(START_GAP, END_GAP)
+    return first_number, second_number
+
+def calculate_gcd(a: int, b: int) -> int:
+    """Calculates the greatest common divisor (GCD) of two numbers
+    using the Euclidean algorithm.
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The greatest common divisor of the two numbers.
+    """
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def get_gameinfo() -> tuple[str, str]:
+    """Generates two random numbers and finds their greatest common divisor (GCD).
+
+    Returns:
+        tuple[str, str]: A tuple of two elements:
+        - a string with two numbers separated by a space (for example, "15 25")
+        - a string with their largest common divisor
+    """
+    first_number, second_number = generate_random_numbers()
     question = f'{first_number} {second_number}'
-
-    while first_number != 0 and second_number != 0:
-        if first_number > second_number:
-            first_number %= second_number
-        else:
-            second_number %= first_number
-
-    answer = first_number + second_number
+    answer = calculate_gcd(first_number, second_number)
     return question, str(answer)
