86b6f4f73c130b1a261928d5787422721d5d4803
Ground <groundgtx@gmail.com>
2025-01-22 21:14:00 +0300

    Refactor gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,31 +5,22 @@ END_GAP = 50
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def generate_random_numbers() -> tuple[int, int]:
-    """Generates two random numbers within the specified range.
-
-    Returns:
-        tuple[int, int]: A tuple containing two random integers.
-    """
-    first_number = randint(START_GAP, END_GAP)
-    second_number = randint(START_GAP, END_GAP)
-    return first_number, second_number
-
-
-def calculate_gcd(a: int, b: int) -> int:
+def calculate_gcd(first_number: int, second_number: int) -> int:
     """Calculates the greatest common divisor (GCD) of two numbers
     using the Euclidean algorithm.
 
     Args:
-        a (int): The first number.
-        b (int): The second number.
+        first_number (int): The first number to check
+        second_number (int): The second number to check
 
     Returns:
         int: The greatest common divisor of the two numbers.
     """
-    while b != 0:
-        a, b = b, a % b
-    return a
+    while second_number != 0:
+        first_number, second_number = (second_number, first_number
+                                       % second_number)
+    result = first_number
+    return result
 
 
 def get_game_objects() -> tuple[str, str]:
@@ -41,6 +32,8 @@ def get_game_objects() -> tuple[str, str]:
         - a string with two numbers separated by a space
         - a string with their largest common divisor
     """
-    first_number, second_number = generate_random_numbers()
-    answer = calculate_gcd(first_number, second_number)
-    return f'{first_number} {second_number}', str(answer)
+    first_number = randint(START_GAP, END_GAP)
+    second_number = randint(START_GAP, END_GAP)
+    question = f'{first_number} {second_number}'
+    answer = str(calculate_gcd(first_number, second_number))
+    return question, str(answer)
