8afe57fcc202f83956ff9a26652400e4d3be8969
Ground <groundgtx@gmail.com>
2025-01-23 21:29:09 +0300

    Addind . in doc to test

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,7 +6,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number: int) -> bool:
-    """Checks number`s prime status'
+    """Checks number`s prime status.
 
     Args:
 
